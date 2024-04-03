3ff4b2d04c2c8dc385d45514c923f05afc515850
lsewcx <lse210085@163.com>
2024-04-03 22:18:04 +0800

    Update infer.py

--- onnruntime-gpu/yolov9/infer.py
+++ onnruntime-gpu/yolov9/infer.py
@@ -44,6 +44,7 @@ class YOLOv9:
         """
         opt_session = onnxruntime.SessionOptions()  # 创建会话选项实例
         opt_session.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL  # 禁用所有图优化
+        providers = [] 
         if self.device.casefold() != "cpu":  # 如果设备不是CPU，则添加CUDA执行提供者
             providers.append("CUDAExecutionProvider")
         session = onnxruntime.InferenceSession(self.model_path, providers=providers)  # 创建会话实例
